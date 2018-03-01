a22bd6d0b8ef11ccee4071583794413e8641efc5
BrangyCastro <brangy.castro1@gmail.com>
2018-02-28 22:08:30 -0500

    Actualizacion 2

--- src/arbolarreglo/ArbolArreglo.java
+++ src/arbolarreglo/ArbolArreglo.java
@@ -7,6 +7,7 @@ package arbolarreglo;
 
 import java.util.Scanner;
 import java.util.*;
+
 /**
  *
  * @author Luis
@@ -16,89 +17,82 @@ public class ArbolArreglo {
     /**
      * @param args the command line arguments
      */
-   
-    public static arbolab.NodoArbol ingresoNodo(){//Para ingresar el nodo por teclado
-        
+    public static arbolab.NodoArbol ingresoNodo() {//Para ingresar el nodo por teclado
+
         int llave = (int) (Math.random() * 100) + 1;
-        Scanner teclado=new Scanner(System.in);
+        Scanner teclado = new Scanner(System.in);
         System.out.println("Ingrese Elemento a guardar en el árbol: ");
-        String valor=teclado.nextLine();
-        arbolab.NodoArbol nodoNuevo=new arbolab.NodoArbol(llave,valor);
+        String valor = teclado.nextLine();
+        arbolab.NodoArbol nodoNuevo = new arbolab.NodoArbol(llave, valor);
         return nodoNuevo;
-        
+
     }
-    
-    public static arbolab.NodoArbol ingresoNodo(String valor1){//Para ingresar el nodo por teclado
-        
+
+    public static arbolab.NodoArbol ingresoNodo(String valor1) {//Para ingresar el nodo por teclado
+
         int llave = (int) (Math.random() * 100) + 1;
         String valor = valor1;
-        arbolab.NodoArbol nodoNuevo=new arbolab.NodoArbol(llave,valor);
+        arbolab.NodoArbol nodoNuevo = new arbolab.NodoArbol(llave, valor);
         return nodoNuevo;
-        
+
     }
 
     public static void main(String[] args) {
         // TODO code application logic here
-        
-         String cadena = "Luis Antonio Auz Garcia";
-    String[] arreglo = cadena.split(" ");
+
+        arbolab.NodoArbol arbol = null;
+        arbolab.NodoArbol nodoNuevo;
+        Scanner teclado = new Scanner(System.in);
+        int opcion;
+
+        String cadena = "Luis Antonio Auz Garcia";
+        String[] arreglo = cadena.split(" ");
         Stack pila = new Stack();
         
-         for(int i =0; i < arreglo.length; i++){   
+        // Pila. Clase preconstruida(Stack).
+        //*****************************************
+        for (int i = 0; i < arreglo.length; i++) {
             pila.push(arreglo[i]);
-     }
-        System.out.println(pila.size());
-        
-        
-        
-        for(int i = 0; i< pila.size(); ++i){
-            System.out.println(pila.pop());
         }
-        arbolab.NodoArbol arbol=null;
-        arbolab.NodoArbol nodoNuevo;
-        Scanner teclado=new Scanner(System.in);
-        int opcion;
-        //Insertar, buscar, modificar, eliminar, imprimir
-        //estaVacia, Salir
-      /*  do{
-            System.out.println(pila.pop());
+        while (!pila.empty()) {
             nodoNuevo = ingresoNodo(pila.pop().toString());
-            arbol=arbolab.NodoArbol.insertarArbol(nodoNuevo, arbol);
-        }while(!pila.empty());*/
-     
-        
-        do{
-            System.out.println("\nIngrese opción: "+
-                                "\n 1. Insertar nodo en árbol"+
-                                " 2. Imprimir árbol en amplitud"+
-                                "\n 3. Impresión Inorden "+
-                                " 4. Impresión Preorden"+
-                                "\n 5. Impresión Postorden"+
-                                " 6. Salir");
-            opcion=teclado.nextInt();
-            switch(opcion)
-            {
-                case 1: nodoNuevo=ingresoNodo();
-                        arbol=arbolab.NodoArbol.insertarArbol(nodoNuevo, arbol);
-                        System.out.println("Ingreso satisfactorio...");
-                        break;
-                case 2: System.out.println("Imprimiendo árbol en amplitud \n");
-                        arbolab.NodoArbol.Anchura2(arbol);
-                        break;
-                case 3: System.out.println("Imprimiendo árbol en Inorden \n");
-                        arbolab.NodoArbol.Inorden(arbol);
-                        break;
-                case 4: System.out.println("Imprimiendo árbol en PreOrden \n");
-                        arbolab.NodoArbol.Preorden(arbol);
-                        break;
-                case 5: System.out.println("Imprimiendo árbol en PostOrden \n");
-                        arbolab.NodoArbol.Postorden(arbol);
-                        break;       
+            arbol = arbolab.NodoArbol.insertarArbol(nodoNuevo, arbol);
+        }
+        //*********************************
+        do {
+            System.out.println("\nIngrese opción: "
+                    + "\n 1. Insertar nodo en árbol"
+                    + " 2. Imprimir árbol en amplitud"
+                    + "\n 3. Impresión Inorden "
+                    + " 4. Impresión Preorden"
+                    + "\n 5. Impresión Postorden"
+                    + " 6. Salir");
+            opcion = teclado.nextInt();
+            switch (opcion) {
+                case 1:
+                    nodoNuevo = ingresoNodo();
+                    arbol = arbolab.NodoArbol.insertarArbol(nodoNuevo, arbol);
+                    System.out.println("Ingreso satisfactorio...");
+                    break;
+                case 2:
+                    System.out.println("Imprimiendo árbol en amplitud \n");
+                    arbolab.NodoArbol.Anchura2(arbol);
+                    break;
+                case 3:
+                    System.out.println("Imprimiendo árbol en Inorden \n");
+                    arbolab.NodoArbol.Inorden(arbol);
+                    break;
+                case 4:
+                    System.out.println("Imprimiendo árbol en PreOrden \n");
+                    arbolab.NodoArbol.Preorden(arbol);
+                    break;
+                case 5:
+                    System.out.println("Imprimiendo árbol en PostOrden \n");
+                    arbolab.NodoArbol.Postorden(arbol);
+                    break;
             }
-            
-        }while(opcion!=6);
-    }
-    
-}
-    
 
+        } while (opcion != 6);
+    }
+
+}
